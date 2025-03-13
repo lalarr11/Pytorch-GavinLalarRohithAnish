2e5bfc0125d956d2788f57485e4c2ba82be4860e
Lalar Rajpurohit <rajpurohit1045@mydusd.org>
2025-03-12 19:50:41 -0700

    Create decoder.py

---
+++ decoder.py
@@ -0,0 +1,12 @@
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, hidden_dim):
+        super(Decoder, self).__init__()
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hidden_dim)
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, input_step, hidden, cell):
+        embedded = self.embedding(input_step)
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
+        prediction = self.fc_out(output.squeeze(0))
+        return prediction, hidden, cell
